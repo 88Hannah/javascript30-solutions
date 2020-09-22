6ca8a17958defbfaecbea78a317a4ed9b9a577fb
88Hannah <hannahsangar@gmail.com>
2020-09-22 13:47:53 +0100

    Web page complete

--- 03 - CSS variables/script.js
+++ 03 - CSS variables/script.js
@@ -1,1 +1,11 @@
-// alert("Hello")
+const inputs = document.querySelectorAll('.controls input');
+
+const handleUpdate = control => {
+    const unit = control.dataset.unit || '';
+    document.documentElement.style.setProperty(`--${control.name}`, control.value + unit);
+};
+
+inputs.forEach(control => {
+    control.addEventListener('change', event => handleUpdate(event.target));
+    control.addEventListener('mousemove', event => handleUpdate(event.target));
+});
